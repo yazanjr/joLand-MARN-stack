15e99c34c80ae91387ae4b69e2ddc6dd8a1ec699
yazanjr <yazaniyadj03@gmail.com>
2025-10-28 21:28:15 +0300

    final render changes

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,2 @@
-export const PORT =  5555;
-
-export const MONGODB_URL ="mongodb+srv://joLand:joLand2025@joland.z0nwiug.mongodb.net/?appName=joLand"
+export const PORT = process.env.PORT || 5555;
+export const MONGODB_URL = process.env.MONGODB_URL;
